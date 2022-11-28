69ca555eb0475f61f8800faf3090d511d7f095d0
mohammadsoheluddin <62943893+mohammadsoheluddin@users.noreply.github.com>
2022-11-28 21:25:30 +0600

    somthing changes

--- src/Pages/Dashboard/Dashboard.js
+++ src/Pages/Dashboard/Dashboard.js
@@ -13,9 +13,9 @@ const Dashboard = () => {
                 <label htmlFor="dashboard-sidebar" className="drawer-overlay"></label>
                 <ul className="menu p-4 overflow-y-auto w-48 bg-base-100 text-base-content">
                     {/* <!-- Sidebar content here --> */}
-                    <li><Link to='/dashboard' >My Appointments</Link></li>
-                    <li><Link to='/dashboard/review' >My Reviews</Link></li>
-                    <li><Link to='/dashboard/history' >My History</Link></li>
+                    <li><Link to='/dashboard' >My Appointments-------</Link></li>
+                    <li><Link to='/dashboard/review' >My Reviews--------</Link></li>
+                    <li><Link to='/dashboard/history' >My History-------</Link></li>
                 </ul>
 
             </div>
